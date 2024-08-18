b8ee949665e26bc648ba4ecc4d08e5877a023aac
Atif-Bashir-1998 <adobe8832289@gmail.com>
2024-08-18 12:57:10 +0500

    basic role page created

--- resources/js/Pages/RolePermission/Role.vue
+++ resources/js/Pages/RolePermission/Role.vue
@@ -1,8 +1,12 @@
 <template>
-    <p>Roles</p>
+    <DefaultLayout title="Role Management">
+        <p>Roles</p>
+    </DefaultLayout>
 </template>
 
 <script setup>
+import DefaultLayout from '@/Layouts/DefaultLayout.vue'
+
 defineProps({
     roles: {
         type: Object,
