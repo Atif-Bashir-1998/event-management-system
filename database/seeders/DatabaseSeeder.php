df23afe26be2a503bc5ee47b6d9d34072a60fd89
Atif-Bashir-1998 <adobe8832289@gmail.com>
2024-08-18 10:15:06 +0500

    updated the DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            RolePermissionSeeder::class,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ])->assignRole('admin');
+
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
