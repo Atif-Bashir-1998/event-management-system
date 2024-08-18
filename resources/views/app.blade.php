dc4b6e706d797b0ef19c7d5c53173294485a0131
Atif-Bashir-1998 <adobe8832289@gmail.com>
2024-08-18 09:50:26 +0500

    added support for multiple themes by daisyui

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <html data-theme="light"></html>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
