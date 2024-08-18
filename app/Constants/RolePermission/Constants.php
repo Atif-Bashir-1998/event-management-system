d57db6ea6119b561f57ba15c24f79edeb1407fca
Atif-Bashir-1998 <adobe8832289@gmail.com>
2024-08-18 08:16:17 +0500

    created constants related to RolePermission feature

---
+++ app/Constants/RolePermission/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constants\RolePermission;
+
+class Constants
+{
+    public const ROLE_CREATE_SUCCESS = 'Role has been created';
+    public const ROLE_CREATE_ERROR = 'Role could not be created';
+    public const ROLE_UPDATE_SUCCESS = 'Role has been updated';
+    public const ROLE_UPDATE_ERROR = 'Can not update the role';
+    public const ROLE_DELETE_SUCCESS = 'Role has been deleted';
+    public const ROLE_DELETE_ERROR = 'Can not delete role with existing users';
+
+}
