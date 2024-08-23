e09383ccd52555ca82d13f6355e714f96b66faa3
Atif-Bashir-1998 <adobe8832289@gmail.com>
2024-08-23 17:17:24 +0500

    added default roles and hierarchy in constants

--- app/Constants/RolePermission/Constants.php
+++ app/Constants/RolePermission/Constants.php
@@ -30,4 +30,15 @@ class Constants
         ],
     ];
 
+    public const DEFAULT_ROLES = [
+        'ADMIN' => 'admin',
+        'ORGANIZER' => 'organizer',
+        'ATTENDEE' => 'attendee'
+    ];
+
+    public const ROLE_HIERARCHY = [
+        'admin' => 2,
+        'organizer' => 1,
+        'attendee' => 0
+    ];
 }
