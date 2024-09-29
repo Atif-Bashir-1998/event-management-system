11f2508ef725d71866ff0535214fff8b1e13a061
Atif-Bashir-1998 <adobe8832289@gmail.com>
2024-09-29 12:01:25 +0500

    provide the "admin" all the permissions without explicitly giving them

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Constants\RolePermission\Constants as RolePermissionConstants;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Implicitly grant "admin" role all permissions
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole(RolePermissionConstants::DEFAULT_ROLES['ADMIN']) ? true : null;
+        });
     }
 }
